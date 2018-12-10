16330a441ac7ce111e1afa631186a8640f63ddf9
PC <test@github.com>
2018-12-10 19:10:28 +0100

    Zadanko

---
+++ Algos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumericalMethods
+{
+    public class Algos
+    {
+        public static double eps = 0.0001;
+		
+        public static List<double> GaussElimination(List<List<double>> input)
+        {
+            var n = input.Count();
+
+            // Print(input);
+
+            var AB = FirstStep(input, n);
+
+            // Print(input);
+
+            var Xs = SecondStep(AB, n);
+
+            // Print(input);
+
+            return Xs;
+        }
+
+        private static List<double> SecondStep(List<List<double>> AB, int n)
+        {
+            var X = new List<double>(new double[n]);
+            for (int i = n - 1; i >= 0; i--)
+            {
+                double s = AB[i][n];
+                for (int j = n - 1; j >= i + 1; j--)
+                {
+                    s -= AB[i][j] * X[j];
+                }
+                if (Math.Abs(AB[i][i]) < eps)
+                {
+
+                }
+                X[i] = s / AB[i][i];
+            }
+            return X;
+        }
+
+        private static List<List<double>> FirstStep(List<List<double>> AB, int n)
+        {
+            double m = 1;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (Math.Abs(AB[i][i]) < eps)
+                    {
+                        new List<List<double>>();
+                    }
+
+                    m = -AB[j][i] / AB[i][i];
+
+                    for (int k = i + 1; k <= n; k++)
+                    {
+                        AB[j][k] += m * AB[i][k];
+                    }
+                }
+            }
+
+            return AB;
+        }
+
+        public static void Print(List<List<double>> input)
+        {
+            foreach (var row in input)
+            {
+                foreach (var el in row)
+                {
+                    Console.Write($"{el} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumericalMethods
+{
+    class Program
+    {
+        public class Input
+        {
+            public List<double> Xs { get; set; } = new List<double>();
+            public List<double> Ys { get; set; } = new List<double>();
+        }
+		
+        public static List<double> Interpolation(Input input)
+        {
+            var n = input.Xs.Count;
+            
+            var matrix = new List<List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix.Add(new List<double>());
+
+                for (int j = 0; j < n; j++)
+                {
+                    var power = n - (j + 1);
+                    var value = Math.Pow(input.Xs[i], power);
+                    matrix.Last().Add(value);
+                }
+
+                matrix.Last().Add(input.Ys[i]);
+            }
+
+           return Algos.GaussElimination(matrix);
+        }
+
+
+        static void Main()
+        {
+            // Interpolacja:
+            // http://www.algorytm.org/procedury-numeryczne/interpolacja-wielomianowa.html
+
+            // Gauss:
+            // https://eduinf.waw.pl/inf/alg/001_search/0076.php
+
+            var args = new Input
+            {
+                Xs = new List<double> {2, 3, 6, 7, 8, 10},
+                Ys = new List<double> {0, 2, 3, 5, 1, 2},
+            };
+
+            var output = Interpolation(args);
+
+            var amount_of_solution = args.Xs.Count() - 1;
+
+            for (int i = amount_of_solution; i >= 0; i--)
+            {
+                string double_position_after_comma = string.Format("{0:0.00}", output[amount_of_solution - i]);
+                Console.Write($"{double_position_after_comma}x^{i} + ");
+            }
+
+			Console.ReadKey();
+        }
+    }
+}
